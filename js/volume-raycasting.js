e60e23571369a23799f1e44d0f81c896e4a8b865
Will Usher <willusher.life@gmail.com>
2018-05-23 09:01:06 -0600

    Load and render the fuel

--- js/volume-raycasting.js
+++ js/volume-raycasting.js
@@ -54,7 +54,7 @@ var fragShader =
 		"discard;" +
 	"}" +
 	"color = vec4(0);" +
-	"highp int n_samples = 8;" +
+	"highp int n_samples = 64;" +
 	"highp float dt = (t_hit.y - t_hit.x) / float(n_samples);" +
 	// TODO: for later when we decided step size based on volume dims
 	//for (highp float t = t_hit.x; t < t_hit.y; t += dt)
@@ -62,7 +62,7 @@ var fragShader =
 		"highp vec3 p = transformed_eye + float(i) * dt * ray_dir;" +
 		"highp float s = texture(volume, p).r;" +
 		"color.rgb += (1.0 - color.a) * s * vec3(s);" +
-		"color.a += (1.0 - color.a) * s * dt;" +
+		"color.a += (1.0 - color.a) * s;" +
 		"if (color.a >= 0.95) {" +
 			"break;" +
 		"}" +
@@ -94,58 +94,58 @@ window.onload = function(){
 
 	var volumeTexture = gl.createTexture();
 	gl.bindTexture(gl.TEXTURE_3D, volumeTexture);
-	gl.texStorage3D(gl.TEXTURE_3D, 1, gl.R8, 4, 4, 4);
+	// TODO: Is non-pow2 or non-even supported?
+	gl.texStorage3D(gl.TEXTURE_3D, 1, gl.R8, 64, 64, 64);
 	gl.texParameteri(gl.TEXTURE_3D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
 	gl.texParameteri(gl.TEXTURE_3D, gl.TEXTURE_WRAP_R, gl.CLAMP_TO_EDGE);
 	gl.texParameteri(gl.TEXTURE_3D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
 	gl.texParameteri(gl.TEXTURE_3D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 
-	var data = [
-		0, 0, 0, 0,
-		0, 0, 0, 0,
-		0, 0, 0, 0,
-		0, 0, 0, 0,
+	//var url = "http://cdn.klacansky.com/open-scivis-datasets/nucleon/nucleon_41x41x41_uint8.raw";
+	//var url = "http://sci.utah.edu/~will/nucleon_41x41x41_uint8.raw"
+	//var url = "file://C:/Users/Will/repos/webgl-volume-raycasting/nucleon_41x41x41_uint8.raw"
+	var url = "file://C:/Users/Will/repos/webgl-volume-raycasting/fuel_64x64x64_uint8.raw"
+	var req = new XMLHttpRequest();
+	req.open("GET", url, true);
+	req.responseType = "arraybuffer";
+	req.onload = function(evt) {
+		console.log("got it");
+		console.log(evt);
+		var dataBuffer = req.response;
+		if (dataBuffer) {
+			dataBuffer = new Uint8Array(dataBuffer);
+			console.log("Got " + dataBuffer.byteLength + " bytes");
+		} else {
+			console.log("no buffer?");
+		}
 
-		0, 0, 0, 0,
-		0, 150, 150, 0,
-		0, 150, 150, 0,
-		0, 0, 0, 0,
-		
-		0, 0, 0, 0,
-		0, 150, 150, 0,
-		0, 150, 150, 0,
-		0, 0, 0, 0,
+		gl.texSubImage3D(gl.TEXTURE_3D, 0, 0, 0, 0, 64, 64, 64,
+			gl.RED, gl.UNSIGNED_BYTE, dataBuffer);
 
-		0, 0, 0, 0,
-		0, 0, 0, 0,
-		0, 0, 0, 0,
-		0, 0, 0, 0
-	];
-	var volData = new Uint8Array(data);
-	gl.texSubImage3D(gl.TEXTURE_3D, 0, 0, 0, 0, 4, 4, 4,
-		gl.RED, gl.UNSIGNED_BYTE, volData);
+		var shader = compileShader(vertShader, fragShader);
+		gl.useProgram(shader);
 
-	var shader = compileShader(vertShader, fragShader);
-	gl.useProgram(shader);
+		var eyePosLoc = gl.getUniformLocation(shader, "eye_pos");
+		var projViewLoc = gl.getUniformLocation(shader, "proj_view");
+		gl.uniform3fv(eyePosLoc, [0.5, 0.5, 1]);
+		var proj = mat4.create();
+		mat4.perspective(proj, 40.0, WIDTH / HEIGHT, 0.01, 100);
+		gl.uniformMatrix4fv(projViewLoc, false, proj);
 
-	var eyePosLoc = gl.getUniformLocation(shader, "eye_pos");
-	var projViewLoc = gl.getUniformLocation(shader, "proj_view");
-	gl.uniform3fv(eyePosLoc, [0.5, 0.5, 1]);
-	var proj = mat4.create();
-	mat4.perspective(proj, 40.0, WIDTH / HEIGHT, 0.01, 100);
-	gl.uniformMatrix4fv(projViewLoc, false, proj);
+		gl.disable(gl.DEPTH_TEST);
+		gl.enable(gl.CULL_FACE);
+		gl.cullFace(gl.FRONT);
+		gl.enable(gl.BLEND);
+		gl.blendFunc(gl.ONE, gl.ONE_MINUS_SRC_ALPHA);
 
-	gl.disable(gl.DEPTH_TEST);
-	gl.enable(gl.CULL_FACE);
-	gl.cullFace(gl.FRONT);
-	gl.enable(gl.BLEND);
-	gl.blendFunc(gl.ONE, gl.ONE_MINUS_SRC_ALPHA);
+		gl.clearColor(0.0, 0.0, 0.0, 1.0);
+		gl.clear(gl.COLOR_BUFFER_BIT);
 
-	gl.clearColor(0.0, 0.0, 0.0, 1.0);
-	gl.clear(gl.COLOR_BUFFER_BIT);
+		gl.drawArrays(gl.TRIANGLE_STRIP, 0, cubeStrip.length / 3);
+		// TODO render every 16ms in a loop
+	};
+	req.send(null);
 
-	gl.drawArrays(gl.TRIANGLE_STRIP, 0, cubeStrip.length / 3);
-	// TODO render every 16ms in a loop
 }
 
 // Compile and link the shaders vert and frag. vert and frag should contain
