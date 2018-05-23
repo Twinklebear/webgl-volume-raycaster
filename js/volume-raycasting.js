87dbd9ba13d8a8310662ecfb1f84f527a44283dc
Will Usher <willusher.life@gmail.com>
2018-05-23 09:08:06 -0600

    correct clear color ;)

--- js/volume-raycasting.js
+++ js/volume-raycasting.js
@@ -138,7 +138,7 @@ window.onload = function(){
 		gl.enable(gl.BLEND);
 		gl.blendFunc(gl.ONE, gl.ONE_MINUS_SRC_ALPHA);
 
-		gl.clearColor(0.0, 0.0, 0.0, 1.0);
+		gl.clearColor(0.0, 0.0, 0.0, 0.0);
 		gl.clear(gl.COLOR_BUFFER_BIT);
 
 		gl.drawArrays(gl.TRIANGLE_STRIP, 0, cubeStrip.length / 3);
