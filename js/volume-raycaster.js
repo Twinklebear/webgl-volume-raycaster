56bda8a9689fdfd9ba6c44d4d9ddcc17fd6da065
Will Usher <willusher.life@gmail.com>
2018-06-01 10:04:22 -0600

    pick sampling rate based on volume dims

--- js/volume-raycaster.js
+++ js/volume-raycaster.js
@@ -20,18 +20,25 @@ var vertShader =
 "layout(location=0) in vec3 pos;" +
 "uniform mat4 proj_view;" +
 "uniform highp vec3 eye_pos;" +
+"uniform highp vec3 volume_scale;" +
 "out vec3 vray_dir;" +
+"flat out highp vec3 transformed_eye;" +
 "void main(void) {" +
-	"vray_dir = pos - eye_pos;" +
-	"gl_Position = proj_view * vec4(pos, 1);" +
+	"transformed_eye = eye_pos / volume_scale;" +
+	// TODO: For non-uniform size volumes we need to transform them differently as well
+	// to center them properly
+	"vray_dir = pos - transformed_eye;" +
+	"gl_Position = proj_view * vec4(pos * volume_scale, 1);" +
 "}";
 
 var fragShader =
 "#version 300 es\n" +
 "uniform highp sampler3D volume;" +
 "uniform highp sampler2D colormap;" +
+"uniform highp ivec3 volume_dims;" +
 "uniform highp vec3 eye_pos;" +
 "in highp vec3 vray_dir;" +
+"flat in highp vec3 transformed_eye;" +
 "out highp vec4 color;" +
 
 "highp vec2 intersectBox(highp vec3 orig, highp vec3 dir) {" +
@@ -60,20 +67,19 @@ var fragShader =
 "}" +
 
 "void main(void) {" +
-	//"color = texture(colormap, gl_FragCoord.xy / vec2(640, 480));" +
 	"highp vec3 ray_dir = normalize(vray_dir);" +
-	"highp vec2 t_hit = intersectBox(eye_pos, ray_dir);" +
+	"highp vec2 t_hit = intersectBox(transformed_eye, ray_dir);" +
 	"color = vec4(0);" +
 	"if (t_hit.x > t_hit.y) {" +
 		"discard;" +
 	"}" +
-	"highp int n_samples = 64;" +
-	"highp float dt = (t_hit.y - t_hit.x) / float(n_samples);" +
+	//"highp int n_samples = 64;" +
+	"highp vec3 dt_vec = 1.0 / (vec3(volume_dims) * abs(ray_dir));" +
+	// TODO: Scale dt by the device power
+	"highp float dt = 1.5 * min(dt_vec.x, min(dt_vec.y, dt_vec.z));" +
 	"highp float offset = wang_hash(int(gl_FragCoord.x + 640.0 * gl_FragCoord.y));" +
-	// TODO: for later when we decided step size based on volume dims
-	//for (highp float t = t_hit.x; t < t_hit.y; t += dt)
-	"highp vec3 p = eye_pos + (t_hit.x + offset * dt) * ray_dir;" +
-	"for (highp int i = 0; i < n_samples; ++i) {" +
+	"highp vec3 p = transformed_eye + (t_hit.x + offset * dt) * ray_dir;" +
+	"for (highp float t = t_hit.x; t < t_hit.y; t += dt) {" +
 		"highp float val = texture(volume, p).r;" +
 		"highp vec4 val_color = vec4(texture(colormap, vec2(val, 0.5)).rgb, val);"+
 		"color.rgb += (1.0 - color.a) * val_color.a * val_color.rgb;" +
@@ -93,14 +99,19 @@ var proj = null;
 var camera = null;
 var projView = null;
 var projViewLoc = null;
+var volumeScaleLoc = null;
+var volumeDimsLoc = null;
 
 var volumes = {
 	"Fuel": "7d87jcsh0qodk78/fuel_64x64x64_uint8.raw",
+	"Neghip": "zgocya7h33nltu9/neghip_64x64x64_uint8.raw",
+	"Hydrogen Atom": "jwbav8s3wmmxd5x/hydrogen_atom_128x128x128_uint8.raw",
+	//"Boston Teapot": "w4y88hlf2nbduiv/boston_teapot_256x256x178_uint8.raw",
+	//"Engine": "ld2sqwwd3vaq4zf/engine_256x256x128_uint8.raw",
 	"Bonsai": "rdnhdxmxtfxe0sa/bonsai_256x256x256_uint8.raw",
 	"Foot": "ic0mik3qv4vqacm/foot_256x256x256_uint8.raw",
 	"Skull": "5rfjobn0lvb7tmo/skull_256x256x256_uint8.raw",
-	"Hydrogen Atom": "jwbav8s3wmmxd5x/hydrogen_atom_128x128x128_uint8.raw",
-	"Neghip": "zgocya7h33nltu9/neghip_64x64x64_uint8.raw",
+	"Aneurism": "3ykigaiym8uiwbp/aneurism_256x256x256_uint8.raw",
 };
 
 var colormaps = {
@@ -115,7 +126,7 @@ var colormaps = {
 var loadVolume = function(file, onload) {
 	console.log("loading " + file);
 	var m = file.match(fileRegex);
-	var vol_dims = [parseInt(m[2]), parseInt(m[3]), parseInt(m[4])];
+	var volDims = [parseInt(m[2]), parseInt(m[3]), parseInt(m[4])];
 	
 	var url = "https://www.dl.dropboxusercontent.com/s/" + file + "?dl=1";
 	var req = new XMLHttpRequest();
@@ -124,7 +135,7 @@ var loadVolume = function(file, onload) {
 	req.open("GET", url, true);
 	req.responseType = "arraybuffer";
 	req.onprogress = function(evt) {
-		var vol_size = vol_dims[0] * vol_dims[1] * vol_dims[2];
+		var vol_size = volDims[0] * volDims[1] * volDims[2];
 		var percent = evt.loaded / vol_size * 100;
 		loadingProgressText.innerHTML = "Loading: " + percent.toFixed(2) + "%";
 	};
@@ -150,19 +161,27 @@ var selectVolume = function() {
 
 	loadVolume(volumes[selection], function(file, dataBuffer) {
 		var m = file.match(fileRegex);
-		var vol_dims = [parseInt(m[2]), parseInt(m[3]), parseInt(m[4])];
+		var volDims = [parseInt(m[2]), parseInt(m[3]), parseInt(m[4])];
 
 		var tex = gl.createTexture();
 		gl.activeTexture(gl.TEXTURE0);
 		gl.bindTexture(gl.TEXTURE_3D, tex);
-		gl.texStorage3D(gl.TEXTURE_3D, 1, gl.R8, vol_dims[0], vol_dims[1], vol_dims[2]);
+		gl.texStorage3D(gl.TEXTURE_3D, 1, gl.R8, volDims[0], volDims[1], volDims[2]);
 		gl.texParameteri(gl.TEXTURE_3D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
 		gl.texParameteri(gl.TEXTURE_3D, gl.TEXTURE_WRAP_R, gl.CLAMP_TO_EDGE);
 		gl.texParameteri(gl.TEXTURE_3D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
 		gl.texParameteri(gl.TEXTURE_3D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 		gl.texSubImage3D(gl.TEXTURE_3D, 0, 0, 0, 0,
-			vol_dims[0], vol_dims[1], vol_dims[2],
+			volDims[0], volDims[1], volDims[2],
 			gl.RED, gl.UNSIGNED_BYTE, dataBuffer);
+
+		gl.uniform3iv(volumeDimsLoc, volDims);
+		var longestAxis = Math.max(volDims[0], Math.max(volDims[1], volDims[2]));
+		var volScale = [volDims[0] / longestAxis, volDims[1] / longestAxis,
+			volDims[2] / longestAxis];
+		console.log(volScale);
+		console.log(volDims);
+		gl.uniform3fv(volumeScaleLoc, volScale);
 
 		if (!volumeTexture) {
 			setInterval(function() {
@@ -233,6 +252,8 @@ window.onload = function(){
 
 	eyePosLoc = gl.getUniformLocation(shader, "eye_pos");
 	projViewLoc = gl.getUniformLocation(shader, "proj_view");
+	volumeScaleLoc = gl.getUniformLocation(shader, "volume_scale");
+	volumeDimsLoc = gl.getUniformLocation(shader, "volume_dims");
 	projView = mat4.create();
 
 	gl.uniform1i(gl.getUniformLocation(shader, "volume"), 0);
@@ -301,6 +322,7 @@ var registerEventHandlers = function(canvas) {
 	});
 
 	canvas.addEventListener("wheel", function(evt) {
+		evt.preventDefault();
 		camera.zoom(-evt.deltaY);
 	});
 
