8c7b3ab61099e1f81372535175ae9fa9692db977
Will Usher <willusher.life@gmail.com>
2019-03-21 20:21:22 -0600

    webgl-utils update

--- js/webgl-util.js
+++ js/webgl-util.js
@@ -387,12 +387,20 @@ Buffer.prototype.clear = function(buf) {
 }
 
 // Various utilities that don't really fit anywhere else
+
+// Parse the hex string to RGB values in [0, 255]
 var hexToRGB = function(hex) {
 	var val = parseInt(hex.substr(1), 16);
 	var r = (val >> 16) & 255;
 	var g = (val >> 8) & 255;
 	var b = val & 255;
 	return [r, g, b];
+}
+
+// Parse the hex string to RGB values in [0, 1]
+var hexToRGBf = function(hex) {
+	var c = hexToRGB(hex);
+	return [c[0] / 255.0, c[1] / 255.0, c[2] / 255.0];
 }
 
 /* The controller can register callbacks for various events on a canvas:
