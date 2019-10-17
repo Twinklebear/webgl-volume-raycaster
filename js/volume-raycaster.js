1531267c1b5c38aa5da930ad50b9a79ff65adae6
Will Usher <will@sci.utah.edu>
2019-10-17 16:17:34 -0600

    srgb correct

--- js/volume-raycaster.js
+++ js/volume-raycaster.js
@@ -267,7 +267,7 @@ window.onload = function(){
 		var colormap = gl.createTexture();
 		gl.activeTexture(gl.TEXTURE1);
 		gl.bindTexture(gl.TEXTURE_2D, colormap);
-		gl.texStorage2D(gl.TEXTURE_2D, 1, gl.RGBA8, 180, 1);
+		gl.texStorage2D(gl.TEXTURE_2D, 1, gl.SRGB8_ALPHA8, 180, 1);
 		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
 		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_R, gl.CLAMP_TO_EDGE);
 		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
